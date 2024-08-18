187d7c64a510686689a0740544f4f1348b269e6b
PPaccioretti <pablopaccioretti@agro.unc.edu.ar>
2024-08-18 13:09:10 -0500

    Check duplicated coords

--- R/mod_kriging_process.R
+++ R/mod_kriging_process.R
@@ -122,7 +122,7 @@ mod_kriging_process_server <- function(id,
       
       myParam <- kriging_param()
       req(myParam$myTgtVar)
-      myKrige <- MiKrige()
+      # myKrige <- MiKrige()
       myBestModel <- MejorModelo()
       myVariogram <- variogram()
       
@@ -179,6 +179,8 @@ mod_kriging_process_server <- function(id,
       file <- dataset()
       file <- check_fix_polygon_multi(file)
       # file <- sf::as_Spatial(file)
+      
+      file <- removeSpatialDuplicated(file, session = session)
       
       autofitVariogram_rep <- repeatable(automap::autofitVariogram, 
                              seed = 169)
@@ -238,7 +240,9 @@ mod_kriging_process_server <- function(id,
       
       file <- dataset()
       file <- check_fix_polygon_multi(file)
-      file <- removeSpatialDuplicated(file, session = session)
+      file <- removeSpatialDuplicated(file, 
+                                      method = 'interpolation',
+                                      session = session)
       coords <- sf::st_coordinates(file)
       colnames(coords) <- c('x', 'y')
       file <- cbind(file, coords)
