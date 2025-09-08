5be45bfba383de83a8606d6c055b8ed465111ef5
PPaccioretti <pablopaccioretti@agro.unc.edu.ar>
2025-09-08 15:48:27 -0300

    Universal Kriging fix

--- R/mod_kriging_process.R
+++ R/mod_kriging_process.R
@@ -4,29 +4,30 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 #' @importFrom magrittr %>%
-mod_kriging_process_ui <- function(id){
+mod_kriging_process_ui <- function(id) {
   ns <- NS(id)
   tagList(
     # tableOutput(ns("TModel"))
   )
 }
-    
+
 #' kriging_process Server Functions
 #'
-#' @noRd 
-mod_kriging_process_server <- function(id,
-                                       dataset,
-                                       kriging_param,
-                                       boundary_poly,
-                                       button) {
-  
-  moduleServer( id, function(input, output, session) {
+#' @noRd
+mod_kriging_process_server <- function(
+  id,
+  dataset,
+  kriging_param,
+  boundary_poly,
+  button
+) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    
+
     myFormulaRefactored <- reactive({
       req(dataset())
       req(kriging_param())
@@ -41,9 +42,8 @@ mod_kriging_process_server <- function(id,
       vars <- attr(stats::terms(myParam$formula), which = "term.labels")
 
       if (length(vars) == 2) {
-        
         coords <- colnames(sf::st_coordinates(file))
-        
+
         vars[1] <- coords[1]
         vars[2] <- coords[2]
         # Get the response
@@ -54,62 +54,69 @@ mod_kriging_process_server <- function(id,
         # Build a formula
         myFormula <- paste(rr, " ~ ", pp)
       }
-      
+
       ###
       stats::as.formula(myFormula)
     })
-    
-    
-    
+
     MiKrige <- eventReactive(button(), {
       req(dataset())
       req(kriging_param())
 
       id <-
-        showNotification(loadingText("Searching for the best model..."),
-                         duration = NULL,
-                         closeButton = FALSE)
+        showNotification(
+          loadingText("Searching for the best model..."),
+          duration = NULL,
+          closeButton = FALSE
+        )
       on.exit(removeNotification(id), add = TRUE)
-      
+
       file <- dataset()
       golem::print_dev('Searching for the model...')
-      
+
       if (nrow(file) > 20000) {
         file <- file[sample(nrow(file), 20000), ]
       }
-      
+
       file <- check_fix_polygon_multi(file)
-      
+
       # file <- sf::as_Spatial(file)
       myParam <- kriging_param()
-      krige_cv <- repeatable(testMultipleModelsKrige, 
-                 seed = 169)
+      krige_cv <- repeatable(testMultipleModelsKrige, seed = 169)
 
-      krige_cv(myFormulaRefactored(),
-               file,
-               myParam$selectedModels,
-               myParam$nmax, 
-               myParam$nmin, 
-               myParam$max_dist,
-               myParam$cressie,
-               session = session)
-      
+      krige_cv(
+        myFormulaRefactored(),
+        file,
+        myParam$selectedModels,
+        myParam$nmax,
+        myParam$nmin,
+        myParam$max_dist,
+        myParam$cressie,
+        session = session
+      )
     })
-    
+
     MejorModelo <- eventReactive(button(), {
       ValidationTable = MiKrige()
-      tryCatch({
-        MyBestModels = names(which.min(apply(ValidationTable[8,], 2, as.numeric)))
-        return(MyBestModels)
-      }, error = function(e) {
-        MyBestModels = colnames(ValidationTable)[1]
-        return(MyBestModels)
-      })
+      tryCatch(
+        {
+          MyBestModels = names(which.min(apply(
+            ValidationTable[8, ],
+            2,
+            as.numeric
+          )))
+          return(MyBestModels)
+        },
+        error = function(e) {
+          MyBestModels = colnames(ValidationTable)[1]
+          return(MyBestModels)
+        }
+      )
     })
-    
+
     variablesForVariogramPlot <- reactive({
       myKrige <- MiKrige()
-      
+
       # req(MiKrige())
       # req(MejorModelo())
       # req(variogram())
@@ -117,17 +124,19 @@ mod_kriging_process_server <- function(id,
       golem::print_dev('Fitting the model...')
 
       id <-
-        showNotification(loadingText("Fitting the best model..."),
-                         duration = NULL,
-                         closeButton = FALSE)
+        showNotification(
+          loadingText("Fitting the best model..."),
+          duration = NULL,
+          closeButton = FALSE
+        )
       on.exit(removeNotification(id), add = TRUE)
-      
+
       myParam <- kriging_param()
       req(myParam$myTgtVar)
       # myKrige <- MiKrige()
       myBestModel <- MejorModelo()
       myVariogram <- variogram()
-      
+
       mo <- cbind(myVariogram$var_model)
       nug <- mo[1, 2]
       mod <- cbind(myVariogram$var_model[, 1:4], myVariogram$sserr)
@@ -138,12 +147,11 @@ mod_kriging_process_server <- function(id,
         Modelo <- mod[2, -4]
       }
       Modelo <- cbind(Modelo, Nugget)
-      
+
       row.names(Modelo) = NULL
       RMSE = myKrige[8, myBestModel][[1]]
       RMSEp = RMSE / mean(dataset()[[myParam$myTgtVar]], na.rm = TRUE) * 100
- 
-      
+
       Modelo = data.frame(
         Modelo,
         "RMSE" = RMSE,
@@ -152,19 +160,30 @@ mod_kriging_process_server <- function(id,
       )
       suppressWarnings({
         Parametros <- paste(
-          c("Model", paste(utils::stack(Modelo[-ncol(Modelo)])[, 2]), "Error (%)"),
-          c(as.character(Modelo[1, 1]), paste(round(
-            utils::stack(Modelo)[, 1], 1
-          ))),
+          c(
+            "Model",
+            paste(utils::stack(Modelo[-ncol(Modelo)])[, 2]),
+            "Error (%)"
+          ),
+          c(
+            as.character(Modelo[1, 1]),
+            paste(round(
+              utils::stack(Modelo)[, 1],
+              1
+            ))
+          ),
           sep = ": ",
           collapse = "\n"
         )
       })
-      
+
       VariogramData <-
-        gstat::variogramLine(myVariogram$var_model, max(myVariogram$exp_var$dist))
+        gstat::variogramLine(
+          myVariogram$var_model,
+          max(myVariogram$exp_var$dist)
+        )
       myVariogram <- myVariogram$exp_var
-      
+
       list(
         'variogramline' = VariogramData,
         'variogramPoint' = myVariogram,
@@ -172,24 +191,23 @@ mod_kriging_process_server <- function(id,
         'parametros' = Parametros
       )
     })
-    
+
     variogram <- eventReactive(button(), {
       # req(dataset())
       # req(kriging_param())
       # req(MejorModelo())
- 
+
       file <- dataset()
       file <- check_fix_polygon_multi(file)
       # file <- sf::as_Spatial(file)
-      
+
       file <- removeSpatialDuplicated(file, session = session)
-      
-      autofitVariogram_rep <- repeatable(automap::autofitVariogram, 
-                             seed = 169)
-      
+
+      autofitVariogram_rep <- repeatable(automap::autofitVariogram, seed = 169)
+
       myParam <- kriging_param()
-      # Si no se quiere validacion cruzada 
-      varioagramModel <- 
+      # Si no se quiere validacion cruzada
+      varioagramModel <-
         autofitVariogram_rep(
           myFormulaRefactored(),
           file,
@@ -197,16 +215,16 @@ mod_kriging_process_server <- function(id,
           # cutoff = 10000,
           cressie = myParam$cressie
         )
-      
+
       # ValoresOutput$Variograma <- varioagramModel
       varioagramModel
     })
-    
+
     Mygr <- eventReactive(button(), {
       req(dataset())
       req(kriging_param())
       req(boundary_poly())
- 
+
       file <- dataset()
       file <- check_fix_polygon_multi(file)
       myParam <- kriging_param()
@@ -214,12 +232,28 @@ mod_kriging_process_server <- function(id,
       Mygr <- sf::st_bbox(boundary_poly()) %>%
         stars::st_as_stars(dx = myParam$cellsize) %>%
         sf::st_crop(boundary_poly())
+
+      formula_refactored <- myFormulaRefactored()
+      vars <- attr(stats::terms(formula_refactored), which = "term.labels")
+
+      if (length(vars) > 1) {
+        Mygr[[vars[1]]] <- sf::st_coordinates(Mygr)[[1]]
+        Mygr[[vars[2]]] <- sf::st_coordinates(Mygr)[[2]]
+      }
+
+      Mygr <- Mygr %>%
+        sf::st_crop(boundary_poly())
+
       ncels <- ncol(Mygr) * nrow(Mygr)
-      
+
       if (ncels > 100000) {
         showNotification(
-          paste('Prediction grid has', ncels , 'cels.',
-          'Please check cell size.'),
+          paste(
+            'Prediction grid has',
+            ncels,
+            'cels.',
+            'Please check cell size.'
+          ),
           id = ns('ncels_high'),
           type = "warning",
           session = session
@@ -227,50 +261,101 @@ mod_kriging_process_server <- function(id,
       }
       Mygr
     })
-    
-    
+
     kriging <- eventReactive(button(), {
       req(dataset())
       req(kriging_param())
       req(Mygr())
 
       id <-
-        showNotification(loadingText("Interpolating..."),
-                         duration = NULL,
-                         closeButton = FALSE)
+        showNotification(
+          loadingText("Interpolating..."),
+          duration = NULL,
+          closeButton = FALSE
+        )
       on.exit(removeNotification(id), add = TRUE)
-      
+
       file <- dataset()
       file <- check_fix_polygon_multi(file)
-      file <- removeSpatialDuplicated(file, 
-                                      method = 'interpolation',
-                                      session = session)
+      file <- removeSpatialDuplicated(
+        file,
+        method = 'interpolation',
+        session = session
+      )
+
+      formula_refactored <- myFormulaRefactored()
+
       coords <- sf::st_coordinates(file)
-      colnames(coords) <- c('x', 'y')
+      vars <- attr(stats::terms(formula_refactored), which = "term.labels")
+      if (length(coords) == 2) {
+        colnames(coords) <- vars
+      }
+
       file <- cbind(file, coords)
       myParam <- kriging_param()
       Mygr <- Mygr()
+
+      # plot(Mygr['y', , ])
+
+      # as.data.frame(Mygr)
+      # tmap::tm_shape(Mygr) +
+      #   tmap::tm_raster(col = 'x')
+
       Modelo <- MejorModelo()
 
-      gstat::krige(
-        formula = myParam$formula, #myParam$formula, #myFormulaRefactored(),
+      predicted_krg <- gstat::krige(
+        formula = formula_refactored, #myParam$formula, #myParam$formula, #myFormulaRefactored(),
         locations = file,
         newdata = Mygr,
-        variogram()$var_model,
+        model = variogram()$var_model,
         nmax = myParam$nmax,
         nmin = myParam$nmin,
         maxdist = myParam$max_dist,
         block = c(myParam$block, myParam$block)
       )
+
+      if (length(vars) > 1) {
+        quantile_pred <- quantile(
+          predicted_krg[['var1.pred']],
+          probs = c(0.01, 0.99),
+          na.rm = TRUE
+        )
+        quantile_var <- quantile(
+          predicted_krg[['var1.var']],
+          probs = c(0.01, 0.99),
+          na.rm = TRUE
+        )
+
+        predicted_krg[['var1.pred']][
+          predicted_krg[['var1.pred']] < quantile_pred[1] &
+            !is.na(predicted_krg[['var1.pred']])
+        ] <- quantile_pred[1]
+        predicted_krg[['var1.pred']][
+          predicted_krg[['var1.pred']] > quantile_pred[2] &
+            !is.na(predicted_krg[['var1.pred']])
+        ] <- quantile_pred[2]
+
+        predicted_krg[['var1.var']][
+          predicted_krg[['var1.var']] < quantile_var[1] &
+            !is.na(predicted_krg[['var1.var']])
+        ] <- quantile_var[1]
+        predicted_krg[['var1.var']][
+          predicted_krg[['var1.var']] > quantile_var[2] &
+            !is.na(predicted_krg[['var1.var']])
+        ] <- quantile_var[2]
+      }
+
+      # plot(pred['var1.pred',,])
+      predicted_krg
+
       # ValoresOutput$kriging <- krigingfit$krige_output
     })
-    
+
     # output$TModel <- renderTable({
     #   req(MiKrige())
     #   miKrige <- as.data.frame(MiKrige())
     # })
-    
-    
+
     list(
       kriging = kriging,
       grid = Mygr,
@@ -290,9 +375,9 @@ mod_kriging_process_server <- function(id,
     )
   })
 }
-    
+
 ## To be copied in the UI
 # mod_kriging_process_ui("kriging_process_ui_1")
-    
+
 ## To be copied in the server
 # mod_kriging_process_server("kriging_process_ui_1")
--- R/mod_kriging_results.R
+++ R/mod_kriging_results.R
@@ -4,143 +4,150 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
 #' @importFrom shiny NS tagList
 mod_kriging_results_ui <- function(id) {
   ns <- NS(id)
   tagList(
     div(
-    id = ns("noInterpolated"),
-    p("No interpolation process was made.")
-  ),
-  shinyjs::hidden(div(
-    id = ns("yesInterpolated"),
-    tagList(
-      bslib::layout_columns(
-        col_widths = c(4, 4, 4, -4, 8),
-        row_heights =  bslib::breakpoints(
-          sm = c('2fr', '2fr', '2fr', '0.75fr'),
-          md = c('2fr','0.80fr'),
-          lg = c('2fr','0.5fr')
-        ),
-        {
-          
-          bslib::card(
-            full_screen = TRUE,
-            bslib::card_header(
-              "Variogram Plot"
-            ),
-            bslib::card_body(
-              shinycssloaders::withSpinner(plotOutput(ns(
-                "VariogramPlot"
-              )))
-            ),
-            bslib::card_footer(
-              btn_dwnd_centered(ns("download_variogram_plot"),
-                                "Download Plot")
+      id = ns("noInterpolated"),
+      p("No interpolation process was made.")
+    ),
+    shinyjs::hidden(div(
+      id = ns("yesInterpolated"),
+      tagList(
+        bslib::layout_columns(
+          col_widths = c(4, 4, 4, -4, 8),
+          row_heights = bslib::breakpoints(
+            sm = c('2fr', '2fr', '2fr', '0.75fr'),
+            md = c('2fr', '0.80fr'),
+            lg = c('2fr', '0.5fr')
+          ),
+          {
+            bslib::card(
+              full_screen = TRUE,
+              bslib::card_header(
+                "Variogram Plot"
+              ),
+              bslib::card_body(
+                shinycssloaders::withSpinner(plotOutput(ns(
+                  "VariogramPlot"
+                )))
+              ),
+              bslib::card_footer(
+                btn_dwnd_centered(
+                  ns("download_variogram_plot"),
+                  "Download Plot"
+                )
+              )
             )
-          )
-          
-          
-        },
-        {
+          },
+          {
             bslib::navset_card_pill(
-              bslib::nav_panel(title = "Predicted Plot", 
-                               shinycssloaders::withSpinner(plotOutput(ns(
-                                 "KrigingPlot"
-                               )))
+              bslib::nav_panel(
+                title = "Predicted Plot",
+                shinycssloaders::withSpinner(plotOutput(ns(
+                  "KrigingPlot"
+                )))
               ),
               bslib::nav_spacer(),
               bslib::nav_panel(
                 shiny::icon("gear"),
                 fillable = FALSE,
-               mod_ggplot_options_ui(ns("ggplot_options_pred"))
+                mod_ggplot_options_ui(ns("ggplot_options_pred"))
               ),
-              bslib::nav_item(btn_dwnd_centered(ns("download_predicted_plot"),
-                                                "Download Plot")),
+              bslib::nav_item(btn_dwnd_centered(
+                ns("download_predicted_plot"),
+                "Download Plot"
+              )),
               placement = "below"
             )
-          
-        },
-        {
-          
-          bslib::navset_card_pill(
-            bslib::nav_panel(title = "Predicted variance", 
-                             shinycssloaders::withSpinner(plotOutput(ns(
-                               "VarKrigingPlot"
-                             )))
-            ),
-            bslib::nav_spacer(),
-            bslib::nav_panel(
-              shiny::icon("gear"),
-              fillable = FALSE,
-              mod_ggplot_options_ui(ns("ggplot_options_var"),
-                                    pallette_selected = 'cm')
-            ),
-            bslib::nav_item(btn_dwnd_centered(ns("download_variance_plot"),
-                                              "Download Plot")),
-            placement = "below"
-          )
-        },
-        {
-          
-          bslib::card(
-            full_screen = FALSE,
-            bslib::card_header(
-              "Download datasets"
-            ),
-            bslib::card_body(
-              
-              bslib::layout_columns(
-                col_widths = c(-2, 4, 4, -2),
-                btn_dwnd_centered(ns("download_pred_tiff"),
-                                  "Download Tif",
-                                  style = 'text-align: center; font-size:100%;'),
-                btn_dwnd_centered(ns("download_pred_gpkg"),
-                                  "Download vector data",
-                                  style = 'text-align: center; font-size:100%;')
+          },
+          {
+            bslib::navset_card_pill(
+              bslib::nav_panel(
+                title = "Predicted variance",
+                shinycssloaders::withSpinner(plotOutput(ns(
+                  "VarKrigingPlot"
+                )))
+              ),
+              bslib::nav_spacer(),
+              bslib::nav_panel(
+                shiny::icon("gear"),
+                fillable = FALSE,
+                mod_ggplot_options_ui(
+                  ns("ggplot_options_var"),
+                  pallette_selected = 'cm'
+                )
+              ),
+              bslib::nav_item(btn_dwnd_centered(
+                ns("download_variance_plot"),
+                "Download Plot"
+              )),
+              placement = "below"
+            )
+          },
+          {
+            bslib::card(
+              full_screen = FALSE,
+              bslib::card_header(
+                "Download datasets"
+              ),
+              bslib::card_body(
+                bslib::layout_columns(
+                  col_widths = c(-2, 4, 4, -2),
+                  btn_dwnd_centered(
+                    ns("download_pred_tiff"),
+                    "Download Tif",
+                    style = 'text-align: center; font-size:100%;'
+                  ),
+                  btn_dwnd_centered(
+                    ns("download_pred_gpkg"),
+                    "Download vector data",
+                    style = 'text-align: center; font-size:100%;'
+                  )
+                )
               )
             )
-          )
-          
-        }
+          }
+        )
       )
-    )
-  )
-  )
+    ))
   )
 }
 
 #' kriging_results Server Functions
 #'
-#' @noRd 
-mod_kriging_results_server <- function(id,
-                                       variablesForVariogramPlot,
-                                       kriging,
-                                       variogram) {
-  moduleServer( id, function(input, output, session){
+#' @noRd
+mod_kriging_results_server <- function(
+  id,
+  variablesForVariogramPlot,
+  kriging,
+  variogram
+) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    
-    myEvent <- bindEvent( reactive({
-      variablesForVariogramPlot()
-      kriging()
-      variogram()
-      }), {
 
-      myRes <- try(kriging(), silent = T)
-      if (inherits(myRes, "try-error") || is.null(myRes)) {
-        shinyjs::show("noInterpolated")
-        shinyjs::hide("yesInterpolated")
-      } else {
-        shinyjs::hide("noInterpolated")
+    myEvent <- bindEvent(
+      reactive({
+        variablesForVariogramPlot()
+        kriging()
+        variogram()
+      }),
+      {
+        myRes <- try(kriging(), silent = T)
+        if (inherits(myRes, "try-error") || is.null(myRes)) {
+          shinyjs::show("noInterpolated")
+          shinyjs::hide("yesInterpolated")
+        } else {
+          shinyjs::hide("noInterpolated")
+        }
+      },
+      ignoreNULL = FALSE
+    )
 
-      }
-    }, ignoreNULL = FALSE)
-    
-    
     observeEvent(is.data.frame(kriging()) || is.null(variogram()), {
-      
       shinyjs::show("yesInterpolated")
       myRes <- try(kriging(), silent = T)
       if (inherits(myRes, "try-error") || is.null(myRes)) {
@@ -150,27 +157,29 @@ mod_kriging_results_server <- function(id,
         shinyjs::hide("noInterpolated")
         shinyjs::show("yesInterpolated")
       }
-      
     })
-    
-    
-    
-    raster_Pred <- reactive({ 
+
+    raster_Pred <- reactive({
+      browser()
       stars::st_as_stars(kriging())
     })
-    
-    
-    
+
     variogramPlot <- reactive({
       myEvent()
       req(variablesForVariogramPlot())
       mydata <- variablesForVariogramPlot()
 
       variogg <- ggplot2::ggplot(data = mydata$variogramline) +
-        ggplot2::geom_point(data = mydata$variogramPoint,
-                            ggplot2::aes(x = dist, y = gamma),
-                            size = 2) +
-        ggplot2::geom_line(ggplot2::aes(x = dist, y = gamma), color = "blue", size = 1.2) +
+        ggplot2::geom_point(
+          data = mydata$variogramPoint,
+          ggplot2::aes(x = dist, y = gamma),
+          size = 2
+        ) +
+        ggplot2::geom_line(
+          ggplot2::aes(x = dist, y = gamma),
+          color = "blue",
+          size = 1.2
+        ) +
         ggplot2::xlab("Distance") +
         ggplot2::ylab("Semi-variance") +
         ggplot2::annotate(
@@ -184,57 +193,60 @@ mod_kriging_results_server <- function(id,
         ) +
         ggplot2::scale_y_continuous(limits = c(0, NA)) +
         ggplot2::ggtitle("Experimental variogram and fitted variogram model")
-      
+
       variogg
     })
-    
+
     output$VariogramPlot <- renderPlot({
       variogramPlot()
     })
     pred_options <- mod_ggplot_options_server("ggplot_options_pred")
     var_options <- mod_ggplot_options_server("ggplot_options_var")
-    
+
     krigingPlot <- reactive({
       myEvent()
       req(kriging())
-      
+
       pred_options <- pred_options()
-   
+
       zmin <- pred_options$min
       zmax <- pred_options$max
-      
+
       pallette <- pred_options$pallette
-     
+
       label_fill <- pred_options$label
       if (is.null(label_fill)) {
         label_fill <- "Predicted values"
       }
-      
-      
-      krigingPlot <- 
-      ggplot2::ggplot() + 
-        stars::geom_stars(data = kriging(), 
-                          ggplot2::aes(fill = var1.pred, x = x, y = y)) +
-        ggplot2::scale_fill_gradientn(colours = pallette,
-                                      na.value = "transparent",
-                                      limits = c(zmin, zmax)) +
+
+      krigingPlot <-
+        ggplot2::ggplot() +
+        stars::geom_stars(
+          data = kriging(),
+          ggplot2::aes(fill = var1.pred, x = x, y = y)
+        ) +
+        ggplot2::scale_fill_gradientn(
+          colours = pallette,
+          na.value = "transparent",
+          limits = c(zmin, zmax)
+        ) +
         ggplot2::labs(fill = label_fill) +
         ggplot2::theme(legend.position = "bottom") +
-        ggplot2::guides(fill = ggplot2::guide_colourbar(
-          barwidth = 17, 
-          label.position = "bottom")) +
+        ggplot2::guides(
+          fill = ggplot2::guide_colourbar(
+            barwidth = 17,
+            label.position = "bottom"
+          )
+        ) +
         ggplot2::coord_equal()
       krigingPlot
-      
     })
-    
-    
+
     output$KrigingPlot <- renderPlot({
       req(krigingPlot())
       krigingPlot()
-        
     })
-    
+
     #VariogramPlot download
     output$download_variogram_plot <- downloadHandler(
       filename = function() {
@@ -252,12 +264,14 @@ mod_kriging_results_server <- function(id,
       },
       content = function(con) {
         req(krigingPlot())
-        ggplot2::ggsave(con, 
-                        plot = krigingPlot(), 
-                        width = 15,
-                        height = 15,
-                        units = "cm",
-                        device = 'png')
+        ggplot2::ggsave(
+          con,
+          plot = krigingPlot(),
+          width = 15,
+          height = 15,
+          units = "cm",
+          device = 'png'
+        )
       }
     )
     output$download_variance_plot <- downloadHandler(
@@ -266,54 +280,58 @@ mod_kriging_results_server <- function(id,
       },
       content = function(con) {
         req(varkrigingPlot())
-        ggplot2::ggsave(con, 
-                        plot = varkrigingPlot(), 
-                        width = 15,
-                        height = 15,
-                        units = "cm",
-                        device = 'png')
+        ggplot2::ggsave(
+          con,
+          plot = varkrigingPlot(),
+          width = 15,
+          height = 15,
+          units = "cm",
+          device = 'png'
+        )
       }
     )
-    
-    
-    
+
     varkrigingPlot <- reactive({
-      
       req(kriging())
-      
+
       var_options <- var_options()
-    
+
       zmin <- var_options$min
       zmax <- var_options$max
-      
+
       pallette <- var_options$pallette
-      
+
       label_fill <- var_options$label
       if (is.null(label_fill)) {
         label_fill <- "Variance of prediction"
       }
-      
-      ggplot2::ggplot() + 
-        stars::geom_stars(data = kriging(), 
-                          ggplot2::aes(fill = var1.var, x = x, y = y)) +
-        ggplot2::scale_fill_gradientn(colours = pallette,
-                                      na.value = "transparent",
-                                      limits = c(zmin, zmax)) +
+
+      ggplot2::ggplot() +
+        stars::geom_stars(
+          data = kriging(),
+          ggplot2::aes(fill = var1.var, x = x, y = y)
+        ) +
+        ggplot2::scale_fill_gradientn(
+          colours = pallette,
+          na.value = "transparent",
+          limits = c(zmin, zmax)
+        ) +
         ggplot2::theme(legend.position = "bottom") +
         ggplot2::labs(fill = label_fill) +
-        ggplot2::guides(fill = ggplot2::guide_colourbar(
-          barwidth = 17, 
-          label.position = "bottom")) +
+        ggplot2::guides(
+          fill = ggplot2::guide_colourbar(
+            barwidth = 17,
+            label.position = "bottom"
+          )
+        ) +
         ggplot2::coord_equal()
-      
     })
-    
+
     output$VarKrigingPlot <- renderPlot({
       req(varkrigingPlot())
       varkrigingPlot()
     })
-    
-    
+
     #Descarga del GeoTiff
     output$download_pred_tiff <- downloadHandler(
       filename = function() {
@@ -321,8 +339,7 @@ mod_kriging_results_server <- function(id,
       },
       content = function(con) {
         Predicted_Tiff <- raster_Pred()
-        stars::write_mdim(Predicted_Tiff, 
-                           con)
+        stars::write_mdim(Predicted_Tiff, con)
         # ,layer = attributes(Predicted_Tiff)$names
       }
     )
@@ -333,16 +350,14 @@ mod_kriging_results_server <- function(id,
       },
       content = function(con) {
         Predicted_sf <- sf::st_as_sf(raster_Pred())
-        sf::write_sf(Predicted_sf, 
-                     con)
+        sf::write_sf(Predicted_sf, con)
       }
     )
-    
   })
 }
-    
+
 ## To be copied in the UI
 # mod_kriging_results_ui("kriging_results_ui_1")
-    
+
 ## To be copied in the server
 # mod_kriging_results_server("kriging_results_ui_1")
