99fa1952f1ddea5c93c41b72bdf85c593aa13d94
PPaccioretti <pablopaccioretti@agro.unc.edu.ar>
2026-04-22 08:02:03 -0300

    Add help text for clustering indices interpretation after plot

--- R/mod_cluster_results.R
+++ R/mod_cluster_results.R
@@ -4,84 +4,101 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
 #' @importFrom shiny NS tagList
 #' @importFrom magrittr %>%
 mod_cluster_results_ui <- function(id) {
-  
   ns <- NS(id)
-  tagList(div(id = ns("noClustered"),
-              p("No cluster process was made.")),
-          shinyjs::hidden(div(
-            id = ns("yesClustered"),
-            shinycssloaders::withSpinner(tagList(fluidRow(
-              bslib::card(
-                full_screen = TRUE,
-                bslib::card_header("Statistical Indices"),
-                bslib::card_body(
-                  min_height = 200,
-                  class = "gap-2 container",
-                  bslib::layout_columns(
-                    col_widths = 6,
-                    plotly::plotlyOutput(ns("GraficoIndicesConglo")),
-                    bslib::layout_columns(
-                      col_widths = 12,
-                      DT::dataTableOutput(ns("TablaIndicesConglo")),
-                      downloadButton(ns("downloadClasification"), "Download Clasification")
-                    )
-                  )
-                )),
-                
-                mod_ggplot_sf_variable_ui(ns('cluster_plot'))
-            )))
-          )))
+  tagList(
+    div(id = ns("noClustered"), p("No cluster process was made.")),
+    shinyjs::hidden(div(
+      id = ns("yesClustered"),
+      shinycssloaders::withSpinner(tagList(fluidRow(
+        bslib::card(
+          full_screen = TRUE,
+          bslib::card_header("Statistical Indices"),
+          bslib::card_body(
+            min_height = 200,
+            class = "gap-2 container",
+            bslib::layout_columns(
+              col_widths = 6,
+              fluidRow(
+                plotly::plotlyOutput(ns("GraficoIndicesConglo")),
+                shiny::helpText(
+                  "Cluster selection criteria: Lower values indicate better clustering for Summary Index, Xie-Beni and Entropy of Partition indices. ",
+                  "For Partition Coefficient, higher values indicate better clustering."
+                )
+              ),
+              bslib::layout_columns(
+                col_widths = 12,
+                DT::dataTableOutput(ns("TablaIndicesConglo")),
+                downloadButton(
+                  ns("downloadClasification"),
+                  "Download Clasification"
+                )
+              )
+            )
+          )
+        ),
+
+        mod_ggplot_sf_variable_ui(ns('cluster_plot'))
+      )))
+    ))
+  )
 }
 
 #' cluster_results Server Functions
 #'
-#' @noRd 
-mod_cluster_results_server <- function(id,
-                                       clusterResults = reactive(NULL),
-                                       variablesUsed = reactive(NULL),
-                                       data_and_cluster = reactive(NULL)) {
-  moduleServer( id, function(input, output, session){
+#' @noRd
+mod_cluster_results_server <- function(
+  id,
+  clusterResults = reactive(NULL),
+  variablesUsed = reactive(NULL),
+  data_and_cluster = reactive(NULL)
+) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
- 
-    
-    observeEvent(is.null(variablesUsed()), {
-      shinyjs::show("noClustered")
-      shinyjs::disable("navzoneval")
-      shinyjs::hide("yesClustered") 
-    },ignoreNULL = FALSE,
-      ignoreInit = TRUE)
-    # myEvent <- 
-    observeEvent(is.null(clusterResults()) &
-                   is.null(data_and_cluster()) ,
-                 {
-                   myRes <- try(clusterResults(), silent = T)
-                   shinyjs::hide("yesClustered")
-                   if (inherits(myRes, "try-error") || is.null(myRes)) {
-                     shinyjs::show("noClustered")
-                     shinyjs::disable("navzoneval")
-                     shinyjs::hide("yesClustered")
-                   } else {
-                     shinyjs::hide("noClustered")
-                     shinyjs::enable("navzoneval")
-                     shinyjs::show("yesClustered")
-                   }
-                 },ignoreNULL = FALSE,
-                 ignoreInit = FALSE)
-    
-    
+
+    observeEvent(
+      is.null(variablesUsed()),
+      {
+        shinyjs::show("noClustered")
+        shinyjs::disable("navzoneval")
+        shinyjs::hide("yesClustered")
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = TRUE
+    )
+    # myEvent <-
+    observeEvent(
+      is.null(clusterResults()) &
+        is.null(data_and_cluster()),
+      {
+        myRes <- try(clusterResults(), silent = T)
+        shinyjs::hide("yesClustered")
+        if (inherits(myRes, "try-error") || is.null(myRes)) {
+          shinyjs::show("noClustered")
+          shinyjs::disable("navzoneval")
+          shinyjs::hide("yesClustered")
+        } else {
+          shinyjs::hide("noClustered")
+          shinyjs::enable("navzoneval")
+          shinyjs::show("yesClustered")
+        }
+      },
+      ignoreNULL = FALSE,
+      ignoreInit = FALSE
+    )
+
     indices <- reactive({
       clusterResults()$indices
     })
-    
+
     summaryResults <- reactive({
       clusterResults()$summaryResults
     })
-    
+
     output$GraficoIndicesConglo <- plotly::renderPlotly({
       # myEvent()
       req(indices())
@@ -93,7 +110,7 @@ mod_cluster_results_server <- function(id,
           scale(indicesClust[, -1]),
           check.names = FALSE
         )
-      
+
       dataIndicesConglomLong <- stats::reshape(
         dataIndicConglWide,
         idvar = "Cluster",
@@ -109,33 +126,35 @@ mod_cluster_results_server <- function(id,
           labels = unique(dataIndicesConglomLong$Index),
           levels = unique(dataIndicesConglomLong$Index)
         )
-      
+
       dataIndicesConglomLong$isSummary <-
         as.numeric(dataIndicesConglomLong$Index == "Summary Index") + 1
-      
-      ggplotCongl <-  
-        ggplot2::ggplot(dataIndicesConglomLong,
-                        ggplot2::aes(x = Cluster, y = Value, color = Index)) +
+
+      ggplotCongl <-
+        ggplot2::ggplot(
+          dataIndicesConglomLong,
+          ggplot2::aes(x = Cluster, y = Value, color = Index)
+        ) +
         ggplot2::geom_point() +
         ggplot2::geom_line(linetype = dataIndicesConglomLong$isSummary) +
-        ggplot2::labs(y = "Standardized value") 
-      
+        ggplot2::labs(y = "Standardized value")
+
       plotly::ggplotly(ggplotCongl) %>%
         plotly::layout(autosize = TRUE)
-      
     })
-    
-    mod_ggplot_sf_variable_server("cluster_plot",
+
+    mod_ggplot_sf_variable_server(
+      "cluster_plot",
       dataset = data_and_cluster,
       indices = indices
     )
-    
+
     # mod_visualize_spatial_data_server("plotclusters",
     #                                   dataset = data_and_cluster,
     #                                   vars = clusterResults,
     #                                   maxMarkerToShow = reactive(1000000)
     # )
-    
+
     output$downloadClasification <- downloadHandler(
       filename = function() {
         paste('Clasification-', Sys.Date(), '.gpkg', sep = '')
@@ -144,11 +163,11 @@ mod_cluster_results_server <- function(id,
         req(data_and_cluster())
         sf::write_sf(
           data_and_cluster(),
-          file)
+          file
+        )
       }
     )
-    
-    
+
     # output$TablaResultadosConglom <- DT::renderDataTable({
     #   DT::datatable(
     #     Clasificacion()$ResultadosConglom,
@@ -157,27 +176,22 @@ mod_cluster_results_server <- function(id,
     #                    paging = FALSE)
     #   )
     # })
-    
+
     output$TablaIndicesConglo <- DT::renderDataTable({
       req(indices())
       indicesClust <- indices()
       DT::datatable(
         indicesClust,
         rownames = FALSE,
-        options = list(searching = FALSE,
-                       paging = FALSE,
-                       info = FALSE)
-      ) %>% 
+        options = list(searching = FALSE, paging = FALSE, info = FALSE)
+      ) %>%
         DT::formatSignif(-1, 4)
     })
-    
-    
-    
   })
 }
-    
+
 ## To be copied in the UI
 # mod_cluster_results_ui("cluster_results_ui_1")
-    
+
 ## To be copied in the server
 # mod_cluster_results_server("cluster_results_ui_1")
